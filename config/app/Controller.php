bfe450517b00fa8e7f4b95039f393807cca12c7a
Alejandro-cj <aloflow_345@hotmail.com>
2024-02-11 14:43:49 -0500

    app

---
+++ config/app/Controller.php
@@ -0,0 +1,17 @@
+<?php
+class Controller{
+    protected $views, $model;
+    public function __construct() {
+        $this->views = new Views();
+        $this->cargarModel();
+    }
+    public function cargarModel()
+    {
+        $model = get_class($this).'Model';
+        $ruta = 'models/' . $model . '.php';
+        if (file_exists($ruta)) {
+            require_once $ruta;
+            $this->model = new $model();
+        }
+    }
+}
