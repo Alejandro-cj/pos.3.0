be5e9c216a7bbb3f081a1a80f09c40daf7132063
Alejandro-cj <aloflow_345@hotmail.com>
2024-02-11 14:46:08 -0500

    conexion

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+define('BASE_URL', 'http://localhost//pos/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', 'flowalejo321');
+define('DBNAME', 'sistema');
+define('CHARSET', '');
+define('TITLE', 'POS VENTA');
+define('MONEDA', 'S/ ');
+define('HOST_SMTP', 'smtp.gmail.com');
+define('USER_SMTP', 'alejandrocj2002@gmail.com');
+define('CLAVE_SMTP', 'yzdjtscjvcjzbxgm');
+define('PUERTO_SMTP', 465);
