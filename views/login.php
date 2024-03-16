e2164da4da748e6a18f6ca172bc2eb1627ff45db
juanka0207 <juancgaviria2003@gmail.com>
2024-03-16 11:10:47 -0500

    login Atualizado

--- views/login.php
+++ views/login.php
@@ -1,26 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--favicon-->
-    <link rel="shortcut icon" type="image/x-icon" href="<?php echo BASE_URL; ?>assets/images/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="PATH_TO_YOUR_FAVICON">
     <!--plugins-->
-    <link href="<?php echo BASE_URL; ?>assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-    <link href="<?php echo BASE_URL; ?>assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-    <link href="<?php echo BASE_URL; ?>assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <link href="PATH_TO_SIMPLEBAR_CSS" rel="stylesheet">
+    <link href="PATH_TO_PERFECT_SCROLLBAR_CSS" rel="stylesheet">
+    <link href="PATH_TO_METISMENU_CSS" rel="stylesheet">
     <!-- loader-->
-    <link href="<?php echo BASE_URL; ?>assets/css/pace.min.css" rel="stylesheet" />
-    <script src="<?php echo BASE_URL; ?>assets/js/pace.min.js"></script>
+    <link href="PATH_TO_PACE_CSS" rel="stylesheet">
+    <script src="PATH_TO_PACE_JS"></script>
     <!-- Bootstrap CSS -->
-    <link href="<?php echo BASE_URL; ?>assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?php echo BASE_URL; ?>assets/css/bootstrap-extended.css" rel="stylesheet">
+    <link href="PATH_TO_BOOTSTRAP_CSS" rel="stylesheet">
+    <link href="PATH_TO_BOOTSTRAP_EXTENDED_CSS" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-    <link href="<?php echo BASE_URL; ?>assets/css/app.css" rel="stylesheet">
-    <link href="<?php echo BASE_URL; ?>assets/css/icons.css" rel="stylesheet">
-    <title><?php echo TITLE . ' - ' . $data['title']; ?></title>
+    <link href="PATH_TO_APP_CSS" rel="stylesheet">
+    <link href="PATH_TO_ICONS_CSS" rel="stylesheet">
+    <title>TITLE - TITLE VARIABLE HERE</title>
 </head>
 
 <body class="bg-login">
@@ -31,7 +31,7 @@
                 <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
                     <div class="col mx-auto">
                         <div class="mb-4 text-center">
-                            <img src="<?php echo BASE_URL; ?>assets/images/logo.png" width="180" alt="" />
+                            <img src="PATH_TO_YOUR_LOGO" width="180" alt="" />
                         </div>
                         <div class="card">
                             <div class="card-body">
@@ -39,7 +39,8 @@
                                     <div class="text-center">
                                         <h3 class="">Iniciar Sesión</h3>
                                     </div>
-                                    <div class="login-separater text-center mb-4"> <span>OR SIGN IN WITH EMAIL</span>
+                                    <div class="login-separater text-center mb-4">
+                                        <span>OR SIGN IN WITH EMAIL</span>
                                         <hr />
                                     </div>
                                     <div class="form-body">
@@ -57,7 +58,8 @@
                                                 </div>
                                                 <span id="errorClave" class="text-danger"></span>
                                             </div>
-                                            <div class="col-md-12 text-end"> <a href="<?php echo BASE_URL . 'principal/forgot'; ?>">Olvidaste tu contraseña?</a>
+                                            <div class="col-md-12 text-end">
+                                                <a href="PATH_TO_FORGOT_PASSWORD">Olvidaste tu contraseña?</a>
                                             </div>
                                             <div class="col-12">
                                                 <div class="d-grid">
@@ -77,12 +79,12 @@
     </div>
     <!--end wrapper-->
     <!-- Bootstrap JS -->
-    <script src="<?php echo BASE_URL; ?>assets/js/bootstrap.bundle.min.js"></script>
+    <script src="PATH_TO_BOOTSTRAP_BUNDLE_JS"></script>
     <!--plugins-->
-    <script src="<?php echo BASE_URL; ?>assets/js/jquery.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>assets/plugins/simplebar/js/simplebar.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>assets/plugins/metismenu/js/metisMenu.min.js"></script>
-    <script src="<?php echo BASE_URL; ?>assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <script src="PATH_TO_JQUERY_JS"></script>
+    <script src="PATH_TO_SIMPLEBAR_JS"></script>
+    <script src="PATH_TO_METISMENU_JS"></script>
+    <script src="PATH_TO_PERFECT_SCROLLBAR_JS"></script>
     <!--Password show & hide js -->
     <script>
         $(document).ready(function() {
@@ -101,12 +103,12 @@
         });
     </script>
     <!--app JS-->
-    <script src="<?php echo BASE_URL; ?>assets/js/app.js"></script>
-    <script src="<?php echo BASE_URL; ?>assets/js/sweetalert2.all.min.js"></script>
+    <script src="PATH_TO_APP_JS"></script>
+    <script src="PATH_TO_SWEETALERT_JS"></script>
     <script>
-        const base_url = '<?php echo BASE_URL; ?>';
+        const base_url = 'BASE_URL_HERE';
     </script>
-    <script src="<?php echo BASE_URL; ?>assets/js/modulos/login.js"></script>
+    <script src="PATH_TO_LOGIN_JS"></script>
 </body>
 
 </html>
